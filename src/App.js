7f96ccd9c98cb758f6e1cd9de909812fd07c61df
Manya-singh2001 <manyasinghsingh16699@gmail.com>
2023-04-15 03:09:43 +0530

    addition of banner , header, nav, navMobile and users section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Banner from "./components/Banner";
+import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [navMobile, setNavMobile] = useState(false)
   return (
     <div >
+      <Header setNavMobile={setNavMobile} />
       <Banner />
+       {/* mobile nav */}
+       <div
+        className={`${
+          navMobile ? 'right-0' : '-right-full'
+        } fixed top-0 bottom-0 w-48 transition-all`}
+      >
+        <NavMobile setNavMobile={setNavMobile} />
+      </div>
     </div>
   );
 }
