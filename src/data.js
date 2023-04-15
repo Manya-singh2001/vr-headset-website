3611b9bb06e79c5d100e3148f551bd25cdaac8df
Manya-singh2001 <manyasinghsingh16699@gmail.com>
2023-04-15 13:58:20 +0530

    addition of AOS, experience, header, headsets, testimonial, testimonialSlider, and video components

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 // import slider images
-import Avatar1 from '../src/assets/img/avt1.png';
-import Avatar2 from '../src/assets/img/avt2.png';
-import Avatar3 from '../src/assets/img/avt3.png';
+import Avatar1 from '../src/assets/avt1.png';
+import Avatar2 from '../src/assets/avt2.png';
+import Avatar3 from '../src/assets/avt3.png';
 
 export const slider = [
   {
